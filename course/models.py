7bd3210c8e8a227f1ec45fc2bb547b130065e73a
Ismael Arce Figueroa <arcefigueroaismael@gmail.com>
2022-10-29 11:22:23 -0300

    rename course

---
+++ course/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=40)
+    code = models.IntegerField()
+    description = RichTextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Course: {self.name} | code: {self.code}"
+
+
+class Homework(models.Model):
+    name = models.CharField(max_length=40)
+    due_date = models.DateField()
+    is_delivered = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.name} | fecha: {self.due_date}"
+
